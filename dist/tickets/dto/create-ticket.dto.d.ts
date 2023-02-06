3c68c96256c933c20d36719c6cc00441fa498e88
philippe <ppereira@simplon.co>
2023-02-06 13:30:36 +0100

    delete dist

--- dist/tickets/dto/create-ticket.dto.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class CreateTicketDto {
-    message: string;
-}
--- dist/tickets/entities/ticket.entity.d.ts
+++
@@ -1,5 +0,0 @@
-import { BaseEntity } from 'typeorm';
-export declare class Ticket extends BaseEntity {
-    id: number;
-    message: string;
-}
