6c2f641e628741cd248201287e6270e69da417cb
philippe <ppereira@simplon.co>
2023-02-10 16:28:28 +0100

    fix port

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { FriendsModule } from './friends/friends.module';
         TypeOrmModule.forRoot({
             type: 'postgres',
             host: process.env.DB_HOST,
-            port: +process.env.DB_PORT,
+            port: +process.env.DB_PORT!,
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
